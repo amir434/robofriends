77d12affb56d0f6bc4feefc739082bcea2af0da2
Amir Akbari <amir.akbari@smartt.com>
2022-12-15 12:49:35 +0300

    Updates

---
+++ precache-manifest.0ecc2c01018a6422fd56eb986044dc74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f20f444bc9535d70271e",
+    "url": "/my-app/static/js/runtime~main.f20f444b.js"
+  },
+  {
+    "revision": "e139ab46614ba93d4124",
+    "url": "/my-app/static/js/main.e139ab46.chunk.js"
+  },
+  {
+    "revision": "aab98431c2fefd02bc84",
+    "url": "/my-app/static/js/2.aab98431.chunk.js"
+  },
+  {
+    "revision": "1fa2d6c7010fd338cdb6",
+    "url": "/my-app/static/js/1.1fa2d6c7.chunk.js"
+  },
+  {
+    "revision": "e139ab46614ba93d4124",
+    "url": "/my-app/static/css/main.1a88d19f.chunk.css"
+  },
+  {
+    "revision": "aab98431c2fefd02bc84",
+    "url": "/my-app/static/css/2.2d084986.chunk.css"
+  },
+  {
+    "revision": "972dde920eb18bdec279cbe6e15b0dc2",
+    "url": "/my-app/index.html"
+  }
+];
